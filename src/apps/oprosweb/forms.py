60e0f6fe566851709aa5779fb5baa927e19fd0cd
xpfxz <xpfxz@bk.ru>
2025-02-07 15:47:00 +0300

    старая signin форма

--- src/apps/oprosweb/forms.py
+++ src/apps/oprosweb/forms.py
@@ -2,6 +2,7 @@ from .models import CustomUser
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ValidationError
 
 
 class SignUpForm(UserCreationForm):
@@ -37,21 +38,12 @@ class SignInForm(AuthenticationForm):
 
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'password'}))
-
+    
     def clean(self):
         cleaned_data = super().clean()
         email = cleaned_data.get('username')
-        password = cleaned_data.get('password')
 
-        try:
-            user = CustomUser.objects.get(email=email)
-        except CustomUser.DoesNotExist: 
-            self.add_error('username', 'Неправильный адрес электронной почты')
-            return cleaned_data
 
-        if not user.check_password(password):
-            self.add_error('password', 'Неправильный пароль')
-            return cleaned_data
+        cleaned_data['email'] = user
 
-        cleaned_data['email'] = user        
         return cleaned_data
